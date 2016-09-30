464582d9b13875330fb6297e4fa065dde3f97dda
Aleksandrs Muravjovs <kavalskij@gmail.com>
2016-09-30 14:18:25 +0300

    Imported project to git

---
+++ Shop/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  Shop
+//
+//  Created by kirill lukyanov on 29.09.16.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+import Foundation
---
+++ Shop/Storage.swift
@@ -0,0 +1,9 @@
+//
+//  Storage.swift
+//  Shop
+//
+//  Created by kirill lukyanov on 29.09.16.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+import Foundation
